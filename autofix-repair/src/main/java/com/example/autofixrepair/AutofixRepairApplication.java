d0a12bebb6088bc62fe644c72de97e9656a41966
Isidora <isidora.oyanedel@usach.cl>
2024-05-16 18:12:08 -0400

    inicio de esto

---
+++ autofix-repair/src/main/java/com/example/autofixrepair/AutofixRepairApplication.java
@@ -0,0 +1,13 @@
+package com.example.autofixrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofixRepairApplication.class, args);
+    }
+
+}
---
+++ autofix-repair/src/test/java/com/example/autofixrepair/AutofixRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofixrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
