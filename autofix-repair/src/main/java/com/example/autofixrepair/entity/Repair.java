4b4b1cfd88e0f061c19eef0eee40d7302ed5cd20
Isidora <isidora.oyanedel@usach.cl>
2024-05-16 22:06:42 -0400

    aprendiendo

---
+++ autofix-repair/src/main/java/com/example/autofixrepair/entity/Repair.java
@@ -0,0 +1,42 @@
+package com.example.autofixrepair.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "record")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Repair {
+    //clase para el historial de reparaciones
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+
+    private String patent; //patente de letras y numeros
+
+    private String admissionDateDayName; //nombre del dia de llegada
+    private int admissionDateDay; //fecha de llega al taller
+    private int admissionDateMonth;
+    private int admissionHour;   //hora de llegada
+
+    private String repairType; //tipo de reparacion/es
+
+
+    private int departureDateDay; //fecha de salida del vehiculo
+    private int departureDateMonth;
+    private int departureHour; //hora de salida, asumo que deberia ser igual a la de llegada
+
+    private int clientDateDay; //fecha en la que el cliente se lleva el vehiculo
+    private int clientDateMonth;
+    private int clientHour; //hora en la que el cliente se lleva el vehiculo
+
+    private double totalAmount;
+}
+
---
+++ autofix-repair/src/main/java/com/example/autofixrepair/repository/RepairRepository.java
@@ -0,0 +1,19 @@
+package com.example.autofixrepair.repository;
+
+import com.example.autofixrepair.entity.Repair;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RepairRepository extends JpaRepository<Repair, Long> {
+        List<Repair> findByPatent(String patent); //no borrar, esta mala
+
+        //obtener un solo registro al cual le puedo hacer un get luego
+        @Query(value = "SELECT * FROM record WHERE record.patent = :patent", nativeQuery = true)
+        Repair findByPatentOne(@Param("patent") String patent);
+
+}
