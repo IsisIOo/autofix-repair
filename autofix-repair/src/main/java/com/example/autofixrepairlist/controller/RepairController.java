8be00922faf6e54aa8e046691c0a4099c0d661e7
Isidora <isidora.oyanedel@usach.cl>
2024-05-24 21:23:19 -0400

    proabdno poniendo api

--- autofix-repair/src/main/java/com/example/autofixrepairlist/controller/RepairController.java
+++ autofix-repair/src/main/java/com/example/autofixrepairlist/controller/RepairController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/repair")
+@RequestMapping("/api/repair")
 @CrossOrigin("*")
 public class RepairController {
     @Autowired
